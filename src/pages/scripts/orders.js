a40a2ad48dc8d4495af084b94dfadb410850e5a6
VinnRe <22-03804@g.batstate-u.edu.ph>
2024-12-15 10:14:40 +0800

    Add place order logic

--- src/pages/scripts/orders.js
+++ src/pages/scripts/orders.js
@@ -7,6 +7,21 @@ document.addEventListener("DOMContentLoaded", function () {
     const cbDeuBtn = document.getElementById("cbDeu");
     const cbTriBtn = document.getElementById("cbTri");
     const body = document.body;
+    const orderForm = document.getElementById("orderForm");
+    const popupSuccess = document.getElementById("popupSuccess");
+
+    // Show popup on form submission
+    orderForm.addEventListener("submit", (event) => {
+        event.preventDefault(); // Prevent actual form submission
+        
+        // Show the success popup
+        popupSuccess.style.display = "flex";
+
+        // Hide the popup after 3 seconds (optional)
+        setTimeout(() => {
+            popupSuccess.style.display = "none";
+        }, 3000);
+    });
 
     popOpenBtn.addEventListener("click", function () {
         popupSection.classList.add("popup__section-open");
